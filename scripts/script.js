ca913b6331008fbd7473413a3adfc002b705f010
Farasha Rahman Meem <farasha.rahman@gmail.com>
2022-02-09 16:44:44 -0500

    phase-1-updates

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function showSection(section) {
+    let el = document.getElementById(section);
+    let sections = document.getElementById("sections").children;
+    for (let i=0; i < sections.length; i++) {
+        if (sections[i].id != section) {
+            sections[i].style.display = "none";
+        }
+        else {
+            sections[i].style.display = "block";
+        }
+    }
+}
